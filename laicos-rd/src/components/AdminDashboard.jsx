46c47702c5bcb0bedd332d839699c794b0cc3e36
Wagner Alcantara <wagneralcantara36@gmail.com>
2024-10-07 23:08:48 -0400

    actualizacion y modal

--- laicos-rd/src/components/AdminDashboard.jsx
+++ laicos-rd/src/components/AdminDashboard.jsx
@@ -6,10 +6,10 @@ import axios from 'axios';
 import Modal from 'react-modal';
 import Header from './Header';
 import Feed from './feed';
-import AdministradoresList from './AdministradoresList'; // Importa el nuevo componente
-import ActividadesList from './ActividadesList'; // Importa el nuevo componente
-import Parroquias from './Parroquia/ParroquiaList'
-import Diocesis from './Diocesis/DiocesisList'
+import AdministradoresList from './AdministradoresList'; 
+import ActividadesList from './ActividadesList';
+import Parroquias from './Parroquia/ParroquiaList';
+import Diocesis from './Diocesis/DiocesisList';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faClipboardList, faChartPie, faChartArea } from '@fortawesome/free-solid-svg-icons';
 import { FiBarChart } from 'react-icons/fi';
@@ -17,8 +17,9 @@ import { FiBarChart } from 'react-icons/fi';
 Modal.setAppElement('#root');
 
 const AdminDashboard = () => {
-  const userRole = Cookies.get('userRole'); // Obtiene el rol del usuario desde las cookies
+  const userRole = Cookies.get('userRole'); 
   const [administradores, setAdministradores] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false); 
   const [currentAdmin, setCurrentAdmin] = useState({
     nombre: '',
     apellido: '',
@@ -31,7 +32,7 @@ const AdminDashboard = () => {
     rolUsuario: 'miembro',
   });
 
-  const [activeComponent, setActiveComponent] = useState('feed'); // Por defecto en 'feed'
+  const [activeComponent, setActiveComponent] = useState('feed'); 
   const navigate = useNavigate();
   const authToken = Cookies.get('authToken');
   const twoFaVerified = Cookies.get('twoFactorVerified');
@@ -39,7 +40,7 @@ const AdminDashboard = () => {
   useEffect(() => {
     if (!twoFaVerified) {
       navigate('/login');
-    } else if (userRole === 'Administrador') { // Solo obtiene administradores si es administrador
+    } else if (userRole === 'Administrador') {
       obtenerAdministradores();
     }
   }, [twoFaVerified, navigate, userRole]);
@@ -57,6 +58,72 @@ const AdminDashboard = () => {
     }
   };
 
+  const handleOpenModal = (admin = null) => {
+    if (admin) {
+      setCurrentAdmin(admin);
+    } else {
+      setCurrentAdmin({
+        nombre: '',
+        apellido: '',
+        email: '',
+        password: '',
+        sexo: 'Seleccionar',
+        celular: '',
+        nacimiento: '',
+        esMiembro: false,
+        rolUsuario: 'miembro',
+      });
+    }
+    setModalIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const handleDelete = async (adminId) => {
+    try {
+      await axios.delete(`http://localhost:3001/api/administradores/${adminId}`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+      obtenerAdministradores(); 
+    } catch (error) {
+      console.error('Error al eliminar administrador:', error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCurrentAdmin({ ...currentAdmin, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (currentAdmin._id) {
+        // Editar administrador existente
+        await axios.patch(`http://localhost:3001/api/administradores/${currentAdmin._id}`, currentAdmin, {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        });
+      } else {
+        // Añadir nuevo administrador
+        await axios.post('http://localhost:3001/api/administradores', currentAdmin, {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        });
+      }
+      obtenerAdministradores();
+      handleCloseModal();
+    } catch (error) {
+      console.error('Error al guardar administrador:', error);
+    }
+  };
+
   const handleComponentChange = (component) => {
     setActiveComponent(component);
   };
@@ -67,15 +134,12 @@ const AdminDashboard = () => {
       <div className="admin-dashboard">
         <div className="sidebar">
           <h2>Administración</h2>
-          {/* Opción de Feed y Actividades siempre visibles */}
           <a href="#" onClick={() => handleComponentChange('feed')}>
             <FontAwesomeIcon icon={faClipboardList} /> Feed
           </a>
           <a href="#" onClick={() => handleComponentChange('Actividades')}>
             <FontAwesomeIcon icon={faChartPie} /> Actividades
           </a>
-
-          {/* Los siguientes campos solo aparecerán si el usuario es administrador */}
           {userRole === 'Administrador' && (
             <>
               <a href="#" onClick={() => handleComponentChange('Administradores')}>
@@ -97,11 +161,105 @@ const AdminDashboard = () => {
           {activeComponent === 'Administradores' && userRole === 'Administrador' && (
             <AdministradoresList
               administradores={administradores}
+              handleOpenModal={handleOpenModal}
+              handleDelete={handleDelete}
             />
           )}
           {activeComponent === 'Parroquias' && userRole === 'Administrador' && <Parroquias />}
-          {activeComponent === 'Diocesis' && userRole === 'Administrador' && <Diocesis/>}
+          {activeComponent === 'Diocesis' && userRole === 'Administrador' && <Diocesis />}
         </div>
+
+        {/* Modal para añadir o editar administradores */}
+        <Modal isOpen={modalIsOpen} onRequestClose={handleCloseModal}>
+          <h2>{currentAdmin._id ? 'Editar Administrador' : 'Añadir Administrador'}</h2>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Nombre:
+              <input
+                type="text"
+                name="nombre"
+                value={currentAdmin.nombre}
+                onChange={handleInputChange}
+                required
+              />
+            </label>
+            <label>
+              Apellido:
+              <input
+                type="text"
+                name="apellido"
+                value={currentAdmin.apellido}
+                onChange={handleInputChange}
+                required
+              />
+            </label>
+            <label>
+              Email:
+              <input
+                type="email"
+                name="email"
+                value={currentAdmin.email}
+                onChange={handleInputChange}
+                required
+              />
+            </label>
+            <label>
+              Contraseña:
+              <input
+                type="password"
+                name="password"
+                value={currentAdmin.password}
+                onChange={handleInputChange}
+                required={!currentAdmin._id} // Solo requerido si es nuevo
+              />
+            </label>
+            <label>
+              Sexo:
+              <select
+                name="sexo"
+                value={currentAdmin.sexo}
+                onChange={handleInputChange}
+              >
+                <option value="Seleccionar">Seleccionar</option>
+                <option value="Masculino">Masculino</option>
+                <option value="Femenino">Femenino</option>
+              </select>
+            </label>
+            <label>
+              Celular:
+              <input
+                type="tel"
+                name="celular"
+                value={currentAdmin.celular}
+                onChange={handleInputChange}
+                required
+              />
+            </label>
+            <label>
+              Fecha de Nacimiento:
+              <input
+                type="date"
+                name="nacimiento"
+                value={currentAdmin.nacimiento}
+                onChange={handleInputChange}
+                required
+              />
+            </label>
+            <label>
+              Rol de Usuario:
+              <select
+                name="rolUsuario"
+                value={currentAdmin.rolUsuario}
+                onChange={handleInputChange}
+              >
+                <option value="miembro">Miembro</option>
+                <option value="Administrador">Administrador</option>
+              </select>
+            </label>
+            <button type="submit">Guardar</button>
+            <button type="button" onClick={handleCloseModal}>Cancelar</button>
+          </form>
+        </Modal>
       </div>
     </>
   ) : (
--- laicos-rd/src/components/AdministradoresList.jsx
+++ laicos-rd/src/components/AdministradoresList.jsx
@@ -1,137 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../css/Sidebar.css';
-import Cookies from 'js-cookie';
-import axios from 'axios';
-import Modal from 'react-modal';
-import Header from './Header';
-import Feed from './feed';
-import AdministradoresList from './AdministradoresList'; // Importa el nuevo componente
-import ActividadesList from './ActividadesList'; // Importa el nuevo componente
-import Parroquias from './Parroquia/ParroquiaList'
-import Diocesis from './Diocesis/DiocesisList'
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faClipboardList, faChartPie, faChartArea } from '@fortawesome/free-solid-svg-icons';
-import { FiBarChart } from 'react-icons/fi';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import '../css/AdministradoresList.css'; // Asegúrate de tener este archivo CSS
 
-Modal.setAppElement('#root');
-
-const AdminDashboard = () => {
-  const userRole = Cookies.get('userRole'); // Obtiene el rol del usuario desde las cookies
-  const [administradores, setAdministradores] = useState([]);
-  const [modalOpen, setModalOpen] = useState(false); // Estado para abrir/cerrar modal
-  const [currentAdmin, setCurrentAdmin] = useState(null); // Estado para el administrador actual
-  const [activeComponent, setActiveComponent] = useState('feed'); // Por defecto en 'feed'
-  const navigate = useNavigate();
-  const authToken = Cookies.get('authToken');
-  const twoFaVerified = Cookies.get('twoFactorVerified');
-
-  useEffect(() => {
-    if (!twoFaVerified) {
-      navigate('/login');
-    } else if (userRole === 'Administrador') { // Solo obtiene administradores si es administrador
-      obtenerAdministradores();
-    }
-  }, [twoFaVerified, navigate, userRole]);
-
-  const obtenerAdministradores = async () => {
-    try {
-      const response = await axios.get('http://localhost:3001/api/administradores', {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-      });
-      setAdministradores(response.data);
-    } catch (error) {
-      console.error('Error al obtener los administradores:', error);
-    }
-  };
-
-  const handleComponentChange = (component) => {
-    setActiveComponent(component);
-  };
-
-  // Función para abrir el modal y setear el administrador actual (para editar o crear nuevo)
-  const handleOpenModal = (admin = null) => {
-    setCurrentAdmin(admin);
-    setModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setModalOpen(false);
-    setCurrentAdmin(null);
-  };
-
-  const handleDelete = async (adminId) => {
-    try {
-      await axios.delete(`http://localhost:3001/api/administradores/${adminId}`, {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-      });
-      obtenerAdministradores(); // Actualiza la lista de administradores
-    } catch (error) {
-      console.error('Error al eliminar el administrador:', error);
-    }
-  };
-
-  return twoFaVerified ? (
+const AdministradoresList = ({ administradores, handleOpenModal, handleDelete }) => {
+  return (
     <>
-      <Header />
-      <div className="admin-dashboard">
-        <div className="sidebar">
-          <h2>Administración</h2>
-          {/* Opción de Feed y Actividades siempre visibles */}
-          <a href="#" onClick={() => handleComponentChange('feed')}>
-            <FontAwesomeIcon icon={faClipboardList} /> Feed
-          </a>
-          <a href="#" onClick={() => handleComponentChange('Actividades')}>
-            <FontAwesomeIcon icon={faChartPie} /> Actividades
-          </a>
-
-          {/* Los siguientes campos solo aparecerán si el usuario es administrador */}
-          {userRole === 'Administrador' && (
-            <>
-              <a href="#" onClick={() => handleComponentChange('Administradores')}>
-                <FontAwesomeIcon icon={faUser} /> Administrador
-              </a>
-              <a href="#" onClick={() => handleComponentChange('Parroquias')}>
-                <FontAwesomeIcon icon={faChartArea} /> Parroquias
-              </a>
-              <a href="#" onClick={() => handleComponentChange('Diocesis')}>
-                <FontAwesomeIcon icon={FiBarChart} /> Diocesis
-              </a>
-            </>
+      <h1>Lista de Administradores</h1>
+      <button className="add-button" onClick={() => handleOpenModal()}>
+        Añadir Administrador
+      </button>
+      <table className="administradores-table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Email</th>
+            <th>Rol</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {administradores.length === 0 ? (
+            <tr>
+              <td colSpan="4" className="no-data">No hay administradores disponibles</td>
+            </tr>
+          ) : (
+            administradores.map((admin) => (
+              <tr key={admin._id}>
+                <td>{admin.nombre} {admin.apellido}</td>
+                <td>{admin.email}</td>
+                <td>{admin.rolUsuario}</td>
+                <td>
+                  <button className="action-button" onClick={() => handleOpenModal(admin)} aria-label={`Editar ${admin.nombre}`}>
+                    <FontAwesomeIcon icon={faEdit} />
+                  </button>
+                  <button className="action-button delete-button" onClick={() => handleDelete(admin._id)} aria-label={`Eliminar ${admin.nombre}`}>
+                    <FontAwesomeIcon icon={faTrash} />
+                  </button>
+                </td>
+              </tr>
+            ))
           )}
-        </div>
-
-        <div className="content">
-          {activeComponent === 'feed' && <Feed />}
-          {activeComponent === 'Actividades' && <ActividadesList />}
-          {activeComponent === 'Administradores' && userRole === 'Administrador' && (
-            <>
-              <AdministradoresList
-                administradores={administradores}
-                handleOpenModal={handleOpenModal} // Pasar la función para abrir el modal
-                handleDelete={handleDelete} // Pasar la función para eliminar
-              />
-              <Modal isOpen={modalOpen} onRequestClose={handleCloseModal}>
-                <h2>{currentAdmin ? 'Editar Administrador' : 'Añadir Administrador'}</h2>
-                {/* Aquí debes insertar el formulario para crear o editar administrador */}
-                <button onClick={handleCloseModal}>Cerrar</button>
-              </Modal>
-            </>
-          )}
-          {activeComponent === 'Parroquias' && userRole === 'Administrador' && <Parroquias />}
-          {activeComponent === 'Diocesis' && userRole === 'Administrador' && <Diocesis />}
-        </div>
-      </div>
+        </tbody>
+      </table>
     </>
-  ) : (
-    <div>
-      <h2>No estás autorizado para ver esta página. Por favor, verifica tu autenticación de dos factores.</h2>
-    </div>
   );
 };
 
-export default AdminDashboard;
+export default AdministradoresList;
